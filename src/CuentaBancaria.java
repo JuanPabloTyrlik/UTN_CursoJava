2633cabe3d0080965697f21984182438dc1be0ad
JuanPabloTyrlik <juan.pablo.tyrlik@gmail.com>
2019-03-30 19:04:48 -0300

    Tipos de Cuenta. Version 1.0

---
+++ src/CuentaBancaria.java
@@ -0,0 +1,26 @@
+public abstract class CuentaBancaria {
+    protected Titular titular;
+    protected double saldo;
+
+    public Titular getTitular() {
+        return titular;
+    }
+
+    public void setTitular(Titular titular) {
+        this.titular = titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void depositar(double monto) {
+        saldo += monto;
+        System.out.println("Deposito realizado");
+        System.out.println("Monto:\t"+monto);
+        System.out.println("Saldo:\t"+saldo);
+    }
+
+    public abstract void extraer(double monto);
+
+}
