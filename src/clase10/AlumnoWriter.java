0e06795e9ce469fae1b6ac5f5aee9dec6c3375ea
JuanPabloTyrlik <juan.pablo.tyrlik@gmail.com>
2019-06-14 11:52:39 -0300

    clase10. Add Enum class

--- src/clase10/AlumnoWriter.java
+++ src/clase10/AlumnoWriter.java
@@ -4,12 +4,13 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.Year;
 
 public class AlumnoWriter {
     public void writeAlumno(Alumno alumno) throws IOException {
         for (String materia : alumno.getMaterias().keySet()) {
-            File aprobados = new File("D:\\Java\\"+materia+"-Aprobados.txt");
-            File desaprobados = new File("D:\\Java\\"+materia+"-Desaprobados.txt");
+            File aprobados = new File(Path.OUTPUT_FOLDER.getPath() + Year.now().toString() + materia + "-Aprobados.txt");
+            File desaprobados = new File(Path.OUTPUT_FOLDER.getPath() + Year.now().toString() + materia + "-Desaprobados.txt");
             BufferedWriter brAprobados = new BufferedWriter(new FileWriter(aprobados,true));
             BufferedWriter brDesaprobados = new BufferedWriter(new FileWriter(desaprobados,true));
             if (alumno.getMaterias().get(materia)>=4) {
